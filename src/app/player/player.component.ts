a95314d8df4d35bf566a5cc127cd0bfb7f5d8285
LakshmanTurlapati <lakshmantvnm@gmail.com>
2023-10-09 20:37:55 +0530

    1.2

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,52 +1,207 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface SpotifyAuthResponse {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  // Add any other expected properties if needed
+}
+interface SpotifyPlaylistTrack {
+  track: {
+      name: string;
+      album: {
+          images: { url: string }[];
+      };
+      uri: string;
+  };
+}
+
+interface SpotifyPlaylistResponse {
+  items: SpotifyPlaylistTrack[];
+}
+interface SpotifyError {
+  message: string;
+}
+interface SpotifyDeviceEvent {
+  device_id: string;
+}
+interface SpotifyPlayerState {
+  context: {
+      uri: string;
+      metadata: any;
+  };
+  disallows: {
+      pausing: boolean;
+      skipping_prev: boolean;
+  };
+  duration: number;
+  paused: boolean;
+  position: number;
+  repeat_mode: number;
+  shuffle: boolean;
+  track_window: {
+      current_track: {
+          album: {
+              uri: string;
+              name: string;
+              images: [{ url: string }];
+              // ... more album properties if needed
+          };
+          artists: [
+              {
+                  uri: string;
+                  name: string;
+              }
+          ];
+          duration_ms: number;
+          uri: string;
+          name: string;
+          // ... more track properties if needed
+      };
+      next_tracks: Array<any>; // Similarly structured to current_track
+      previous_tracks: Array<any>; // Similarly structured to current_track
+  };
+}
+
+
+
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent {
-  currentIndex = 0;
-  isPlaying = false;
-  progress = 0; 
-  
-  constructor() {
-    this.audio.addEventListener('timeupdate', () => {
-      this.progress = (this.audio.currentTime / this.audio.duration) * 100;
-    });
-  }
+export class PlayerComponent implements OnInit {
+    currentIndex = 0;
+    isPlaying = false;
+    progress = 0;
+    songs: any[] = [];
+    deviceId!: string;
 
-  songs = [
-    { 
-      title: 'Sunflower', 
-      url: 'assets/media/audio/sunflower.mp3', 
-      coverArt: 'assets/media/cover/sunflower.jpeg' 
-    },
-    //... other songs ...
-  ];
-  
 
-  audio = new Audio();
+    // Spotify configurations
+    clientId = 'REMOVED_SPOTIFY_CLIENT_ID';
+    clientSecret = 'REMOVED_SPOTIFY_CLIENT_SECRET';
+    token: string = '';
+    player: any;
 
-  playPause() {
-    if (this.isPlaying) {
-      this.audio.pause();
-    } else {
-      this.audio.src = this.songs[this.currentIndex].url;
-      this.audio.play();
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        this.authenticateAndSetupPlayer();
     }
-    this.isPlaying = !this.isPlaying;
-  }
 
-  next() {
-    this.currentIndex = (this.currentIndex + 1) % this.songs.length;
-    this.audio.src = this.songs[this.currentIndex].url;
-    if (this.isPlaying) this.audio.play();
+    authenticateAndSetupPlayer() {
+      // Fetch the access token
+      this.http.post<SpotifyAuthResponse>('https://accounts.spotify.com/api/token', 
+          'grant_type=client_credentials',
+          {
+              headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                  'Authorization': 'Basic ' + btoa(this.clientId + ':' + this.clientSecret)
+              }
+          }
+      ).subscribe(data => {
+          this.token = data.access_token;
+          this.setupSpotifyPlayer();
+          this.fetchPlaylistDetails();
+      });
   }
+  
 
-  previous() {
-    this.currentIndex = (this.currentIndex - 1 + this.songs.length) % this.songs.length;
-    this.audio.src = this.songs[this.currentIndex].url;
-    if (this.isPlaying) this.audio.play();
+  fetchPlaylistDetails() {
+    const playlistId = '37i9dQZF1DXcrFZ8UTtxv9';
+    this.http.get<SpotifyPlaylistResponse>(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+        headers: {
+            'Authorization': 'Bearer ' + this.token
+        }
+    }).subscribe(data => {
+        this.songs = data.items.map(item => ({
+            title: item.track.name,
+            coverArt: item.track.album.images[0].url,
+            uri: item.track.uri
+        }));
+    });
+}
+
+
+    setupSpotifyPlayer() {
+        if (window['Spotify']) {
+            this.initializePlayer();
+        } else {
+            setTimeout(() => this.setupSpotifyPlayer(), 1000);
+        }
+    }
+
+    initializePlayer() {
+      this.player = new window['Spotify'].Player({
+          name: 'Your Angular Spotify Player',
+          getOAuthToken: (cb: (token: string) => void) => { cb(this.token); }
+      });
+  
+      // Error handling
+      this.player.addListener('initialization_error', ({ message }: SpotifyError) => { console.error(message); });
+      this.player.addListener('authentication_error', ({ message }: SpotifyError) => { console.error(message); });
+      this.player.addListener('account_error', ({ message }: SpotifyError) => { console.error(message); });
+      this.player.addListener('playback_error', ({ message }: SpotifyError) => { console.error(message); });
+  
+      // Playback status updates
+      this.player.addListener('player_state_changed', (state: SpotifyPlayerState) => {
+        console.log(state);
+    });
+    
+  
+      // Ready
+      this.player.addListener('ready', ({ device_id }: SpotifyDeviceEvent) => {
+        console.log('Ready with Device ID', device_id);
+        this.deviceId = device_id;
+    });
+    
+  
+      // Not Ready
+    this.player.addListener('not_ready', ({ device_id }: SpotifyDeviceEvent) => {
+    console.log('Device ID has gone offline', device_id);
+});
+
+  
+      // Connect to the player
+      this.player.connect();
   }
+  
+
+    playPause() {
+        if (this.isPlaying) {
+            this.player.pause().then(() => {
+                console.log('Paused Playback');
+                this.isPlaying = false;
+            });
+        } else {
+            this.player.resume().then(() => {
+                console.log('Resumed Playback');
+                this.isPlaying = true;
+            });
+        }
+    }
+
+    playTrack(uri: string) {
+        this.http.put(`https://api.spotify.com/v1/me/player/play?device_id=${this.deviceId}`, 
+        { uris: [uri] }, 
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${this.token}`
+            }
+        }).subscribe();
+    }
+
+    next() {
+        this.currentIndex = (this.currentIndex + 1) % this.songs.length;
+        this.playTrack(this.songs[this.currentIndex].uri);
+    }
+
+    previous() {
+        this.currentIndex = (this.currentIndex - 1 + this.songs.length) % this.songs.length;
+        this.playTrack(this.songs[this.currentIndex].uri);
+    }
 }
---
+++ src/spotify.d.ts
@@ -0,0 +1,7 @@
+export {};  // This turns the file into a module
+
+declare global {
+    interface Window {
+        Spotify: any;
+    }
+}
